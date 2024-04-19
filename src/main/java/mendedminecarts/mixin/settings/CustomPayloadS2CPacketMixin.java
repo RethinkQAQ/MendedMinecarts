1af478e075af9356150169f1709b0f8960ce88d9
2No2Name <2no2name@web.de>
2024-04-19 12:54:26 +0200

    Inject MendedMinecarts Settings packet at construction

--- src/main/java/mendedminecarts/mixin/settings/CustomPayloadS2CPacketMixin.java
+++ src/main/java/mendedminecarts/mixin/settings/CustomPayloadS2CPacketMixin.java
@@ -5,7 +5,6 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import mendedminecarts.settings.SettingSync;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,11 +18,13 @@ public class CustomPayloadS2CPacketMixin {
             at = @At(
                     value = "INVOKE",
                     remap = false,
-                    target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;"
+                    target = "Lcom/google/common/collect/ImmutableMap;builder()Lcom/google/common/collect/ImmutableMap$Builder;",
+                    ordinal = 0
             )
     )
-    private static ImmutableMap<Identifier, PacketByteBuf.PacketReader<? extends CustomPayload>> put_MendedMinecartsSettings(ImmutableMap.Builder<Identifier, PacketByteBuf.PacketReader<? extends CustomPayload>> instance, Operation<ImmutableMap<Identifier, PacketByteBuf.PacketReader<? extends CustomPayload>>> original) {
+    private static ImmutableMap.Builder<Identifier, PacketByteBuf.PacketReader<?>> put_MendedMinecartsSettings(Operation<ImmutableMap.Builder<Identifier, PacketByteBuf.PacketReader<?>>> original) {
+        ImmutableMap.Builder<Identifier, PacketByteBuf.PacketReader<?>> instance = original.call();
         instance.put(SettingSync.CHANNEL, SettingSync.MendedMinecartsSettingPayload::new);
-        return original.call(instance);
+        return instance;
     }
 }
